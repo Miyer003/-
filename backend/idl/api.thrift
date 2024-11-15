eeeae0bf0dfeca42c5b8896bb0690fced806c2b2
SchwarzSail <1424928981@qq.com>
2024-11-15 12:18:43 +0800

    organize directory structure

--- backend/idl/api.thrift
+++
@@ -1,6 +0,0 @@
-namespace go api
-
-
-service HelloService {
-    string Hello(1: string name) (api.get="/hello");
-}
--- backend/idl/model.thrift
+++
@@ -1,6 +0,0 @@
-namespace go model
-
-struct BaseResp {
-    1: i64 code,
-    2: string msg,
-}
