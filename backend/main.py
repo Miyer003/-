db234b75124be0f782708c1ecea538e1655dcc73
梦游大观园 <3185903988@qq.com>
2024-11-23 15:24:47 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from chat.chat import chat_module
 app = Flask(__name__)
 # 注册blueprint
 app.register_blueprint(chat_module)
+app.register_blueprint(translate_cmp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
