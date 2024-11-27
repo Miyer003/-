a975c8b7442897eaae8afc3071ae8ef877fb8b43
SchwarzSail <119314702+SchwarzSail@users.noreply.github.com>
2024-11-28 00:11:22 +0800

    add frontend deployment and server.yaml for backend deployment (#33)
    * add frontend deployment CD and update server.yaml
    
    * fix some frontend error
    
    * change deployment location
    
    * add publicPath in vue.config.js
    
    * add translate_module
    
    * modify npm ci to npm install

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from chat.chat import chat_module
 from translate.translate_cmp import translate_cmp
+from translate.translate import translate_module
 app = Flask(__name__)
 # 注册blueprint
 app.register_blueprint(chat_module)
 app.register_blueprint(translate_cmp)
+app.register_blueprint(translate_module)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
