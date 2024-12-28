24659f67e35f2c7f2fcde54b395b88d1786468b7
梦游大观园 <3185903988@qq.com>
2024-12-28 20:18:06 +0800

    日志 (#51)
    * Create record
    
    * Rename record to record.py
    
    * Update record.py
    
    * Update record.py
    
    * Update record.py
    
    * Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from translate.translate_cmp import translate_cmp
 from translate.translate import translate_module
 from user.user import user
 from flask_cors import CORS
+from record.record import record
 app = Flask(__name__)
 CORS(app, supports_credentials=True, resources=r'/*')
 # 注册blueprint
@@ -11,6 +12,7 @@ app.register_blueprint(chat_module)
 app.register_blueprint(translate_cmp)
 app.register_blueprint(translate_module)
 app.register_blueprint(user)
+app.register_blueprint(record)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
